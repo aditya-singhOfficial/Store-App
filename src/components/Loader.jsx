bca592e07e246a80e4635090a691df8f40651b8e
Aditya Singh <1211821022@ngpp.ac.in>
2025-11-24 01:07:04 +0530

    Add Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="fixed w-full h-screen bg-gray-700 flex justify-center items-center">
+      <h1 className="text-5xl">Loading...</h1>
+    </div>
+  );
+};
+
+export default Loader;
