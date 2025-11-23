87dacb3d7ea9e2c1e53650662a73d8cfa222f48d
Aditya Singh <1211821022@ngpp.ac.in>
2025-11-24 01:07:54 +0530

    Adds components in App and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductsSection from "./components/ProductsSection";
+import { Route, Routes } from "react-router-dom";
+import Details from "./components/Details";
+import Routing from "./utils/Routing";
 
 const App = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex">
+      <Navbar />
+      {/* <ProductsSection /> */}
+      <Routing />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
