71124a01412ad7f858facbf54e019eac12e8fd69
Aditya Singh <1211821022@ngpp.ac.in>
2025-11-26 01:57:25 +0530

    Adds Edit Product Component

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -3,6 +3,7 @@ import ProductsSection from "../components/ProductsSection";
 import Details from "../components/Details";
 import Category from "../components/Category";
 import AddProduct from "../components/AddProduct";
+import EditProduct from "../components/EditProduct";
 
 const Routing = () => {
   return (
@@ -11,6 +12,7 @@ const Routing = () => {
       <Route path="/products/:id" element={<Details />} />
       <Route path="/category" element={<Category />} />
       <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/edit-product/:id" element={<EditProduct />} />
     </Routes>
   );
 };
